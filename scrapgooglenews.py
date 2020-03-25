feccc9672b5e78d92118b3d1f8f144552abcde37
RumilShah94 <53642463+RumilShah94@users.noreply.github.com>
2020-03-25 22:32:52 +0530

    Add files via upload

---
+++ scrapgooglenews.py
@@ -0,0 +1,21 @@
+
+
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+
+news_url="https://news.google.com/news/rss"
+Client=urlopen(news_url)
+xml_page=Client.read()
+Client.close()
+
+soup_page=soup(xml_page,"xml")
+news_list=soup_page.findAll("item")
+
+for i in news_list:
+  print("NEWS TITLE : \n"+i.title.text+"\n")
+  print("LINK : ")
+  print(i.link.text+"\n")
+  print("PUBLISH DATE :\n "+i.pubDate.text+"\n")
+  print("-"*50+"\n")
+
